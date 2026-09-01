373be92290e8bd0e95f6638519418510b3a54044
matheusyanase95-web <matheus.yanase95@gmail.com>
2026-09-01 20:41:55 -0300

    atividade 006 0109

--- 006-atividade.js
+++ 006-atividade.js
@@ -1,5 +1,29 @@
 let entrada = require("prompt-sync");
 
-let nome = "Pokemon";
-let nunber = "Nota de Avaliação 0 a 10";
+let jogo = entrada ("Digite o nome de um jogo:");
+let jogo = entrada();
+let nota = entrada ("que nota voce dar para esse jogo (0 a 10):");
+let nota = entrada();
+let recomendacao = entrada ("voce recomendaria? (sim/não):");
+let recomendacao = entrada();
+
+//eu controlo o formato
+let bilheteria;
+let numpaisesexibidos = null;
+let notaInt = parseInt(nota, 10);
+let recomendaBool = recomenda === 'sim' ? true : false;
+let ehNulo = numPaisesExibidos === null ? 'null' : 'outra coisa';
+// undefifined
+// null
+
+console.log(" ");
+console.log('------------------------');
+console.log('nome' typeof (nome));
+console.log('nota' typeof (nota));
+//retorno de null é object
+console.log('recomendacao' typeof (recomedacao));
+console.log('notaInt:' + typeof(notaInt));
+console.log('recomendaBool' + typeof(recomendaBool));
+
+entrada();
 
